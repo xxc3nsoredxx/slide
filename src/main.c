23840a35c6adca8de75815b47103e22f53af9523
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2018-04-03 22:33:36 -0500

    Cycles between slides

--- src/main.c
+++ src/main.c
@@ -33,6 +33,7 @@ unsigned int position (uint32_t row, uint32_t col) {
 
 int main (int argc, char **argv) {
     int ret = 0;
+    int slide_count;
     int fb_file;
     int cx;
     unsigned int *fb_buf;
@@ -40,26 +41,32 @@ int main (int argc, char **argv) {
     struct fb_fix_screeninfo finfo;
     uint32_t row;
     uint32_t col;
-    FILE *pic_file;
+    char *fname;
+    FILE **pic_file;
     WINDOW *main_win;
     uint8_t png_sig [8];
     ssize_t nread;
-    png_structp png_ptr = 0;
-    png_infop info_ptr = 0;
-    png_infop end_info = 0;
-    png_bytep *row_pointers;
+    png_structp *png_ptr;
+    png_infop *info_ptr;
+    png_infop *end_info;
+    png_bytep **row_pointers;
     png_byte color_type;
     png_byte bit_depth;
     uint32_t height;
     uint32_t width;
+    int cx2;
+    int quit = 0;
 
     /* Test arguments */
-    if (argc != 2) {
-        printf ("Usage: %s dir\n", *argv);
-        printf ("   dir - path where the slideshow is\n");
-        printf ("         0.png, 1.png, ...\n");
+    if (argc != 3) {
+        printf ("Usage: %s dir count\n", *argv);
+        printf ("   dir     path where the slideshow is\n");
+        printf ("           0.png, 1.png, ...\n");
+        printf ("   count   number of slides\n");
         return -1;
     }
+
+    slide_count = atoi (*(argv + 2));
 
     /* Attempt to open the framebuffer */
     fb_file = open ("/dev/fb0", O_RDWR);
@@ -95,13 +102,6 @@ int main (int argc, char **argv) {
     /* Get the bits per pixel */
     bpp = info.bits_per_pixel;
 
-    /* Open an image file */
-    pic_file = fopen ("../0.png", "rb");
-    if (!pic_file) {
-        ERROR(OPEN_ERR_MSG);
-        return -1;
-    }
-
     /* Start ncurses */
     main_win = initscr ();
     raw ();
@@ -111,102 +111,156 @@ int main (int argc, char **argv) {
     move (0,0);
     refresh ();
 
-    /* Read the image */
-    nread = fread (png_sig, 1, 8, pic_file);
-    if (png_sig_cmp (png_sig, 0, nread)) {
-        ERROR(NOT_PNG_MSG);
-        ret = -1;
-        goto close_ncurses;
-    }
-    png_ptr = png_create_read_struct (PNG_LIBPNG_VER_STRING, 0, 0, 0);
-    if (!png_ptr) {
-        ERROR(PNG_PTR_MSG);
-        ret = -1;
-        goto close_ncurses;
-    }
-    info_ptr = png_create_info_struct (png_ptr);
-    if (!info_ptr) {
-        ERROR(INFO_PTR_MSG);
-        ret = -1;
-        goto close_png;
-    }
-    end_info = png_create_info_struct (png_ptr);
-    if (!info_ptr) {
-        ERROR(INFO_PTR_MSG);
-        ret = -1;
-        goto close_png;
-    }
-
-    /* If libong errors, it comes here */
-    if (setjmp (png_jmpbuf (png_ptr))) {
-        ERROR(LIBPNG_ERR_MSG);
-        goto close_png;
-    }
-
-    /* Initialize IO */
-    png_init_io (png_ptr, pic_file);
-
-    /* Tell libpng about the signature bytes read */
-    png_set_sig_bytes (png_ptr, nread);
-
-    /* Read PNG info */
-    png_read_info (png_ptr, info_ptr);
-
-    /* Get the image dimensions */
-    height = png_get_image_height (png_ptr, info_ptr);
-    width = png_get_image_width (png_ptr, info_ptr);
-
-    color_type = png_get_color_type (png_ptr, info_ptr);
-    bit_depth = png_get_bit_depth (png_ptr, info_ptr);
-
-    /* png_set_interlace_handling (png_ptr); */
-    png_read_update_info (png_ptr, info_ptr);
-    bpp = info.bits_per_pixel;
-
-    /* Initialize the rows */
-    row_pointers = malloc (height * sizeof (png_bytep));
-    for (row = 0; row < height; row++) {
-        *(row_pointers + row) = malloc (png_get_rowbytes (png_ptr, info_ptr));
-    }
-
-    /* Read the png */
-    png_read_image (png_ptr, row_pointers);
-
-    /* Draw to the screen */
-    for (row = 0; row < height && row < info.yres; row++) {
-        cx = 0;
-        for (col = 0; col < width && col < info.xres; col++) {
-            /* R */
-            *(fb_buf + position (row, col)) |= (*(*(row_pointers + row) + (cx + 0)) << 16) & 0xFF0000;
-            /* G */
-            *(fb_buf + position (row, col)) |= (*(*(row_pointers + row) + (cx + 1)) << 8) & 0xFF00;
-            /* B */
-            *(fb_buf + position (row, col)) |= *(*(row_pointers + row) + (cx + 2)) & 0xFF;
-            cx += 3;
+    /* Open image files */
+    fname = malloc (1001);
+    pic_file = malloc (slide_count * sizeof (FILE *));
+    png_ptr = malloc (slide_count * sizeof (png_structp));
+    info_ptr = malloc (slide_count * sizeof (png_infop));
+    end_info = malloc (slide_count * sizeof (png_infop));
+    row_pointers = malloc (slide_count * sizeof (png_bytep *));
+    for (cx = 0; cx < slide_count; cx++) {
+        memset (fname, 0, 1001);
+        sprintf (fname, "%s%d.png", *(argv + 1), cx);
+        *(fname + 1000) = 0;
+        *(pic_file + cx) = fopen (fname, "rb");
+        if (!*(pic_file + cx)) {
+            ERROR(OPEN_ERR_MSG);
+            return -1;
         }
+
+        /* Read the image */
+        nread = fread (png_sig, 1, 8, *(pic_file + cx));
+        if (png_sig_cmp (png_sig, 0, nread)) {
+            ERROR(NOT_PNG_MSG);
+            ret = -1;
+            goto close_ncurses;
+        }
+        *(png_ptr + cx) = png_create_read_struct (PNG_LIBPNG_VER_STRING, 0, 0, 0);
+        if (!*(png_ptr + cx)) {
+            ERROR(PNG_PTR_MSG);
+            ret = -1;
+            goto close_ncurses;
+        }
+        *(info_ptr + cx) = png_create_info_struct (*(png_ptr + cx));
+        if (!*(info_ptr + cx)) {
+            ERROR(INFO_PTR_MSG);
+            ret = -1;
+            goto close_png;
+        }
+        *(end_info + cx) = png_create_info_struct (*(png_ptr + cx));
+        if (!*(end_info + cx)) {
+            ERROR(INFO_PTR_MSG);
+            ret = -1;
+            goto close_png;
+        }
+
+        /* If libong errors, it comes here */
+        if (setjmp (png_jmpbuf (*(png_ptr + cx)))) {
+            ERROR(LIBPNG_ERR_MSG);
+            goto close_png;
+        }
+
+        /* Initialize IO */
+        png_init_io (*(png_ptr + cx), *(pic_file + cx));
+
+        /* Tell libpng about the signature bytes read */
+        png_set_sig_bytes (*(png_ptr + cx), nread);
+
+        /* Read PNG info */
+        png_read_info (*(png_ptr + cx), *(info_ptr + cx));
+
+        /* Get the image dimensions */
+        height = png_get_image_height (*(png_ptr + cx), *(info_ptr + cx));
+        width = png_get_image_width (*(png_ptr + cx), *(info_ptr + cx));
+
+        color_type = png_get_color_type (*(png_ptr + cx), *(info_ptr + cx));
+        bit_depth = png_get_bit_depth (*(png_ptr + cx), *(info_ptr + cx));
+
+        png_read_update_info (*(png_ptr + cx), *(info_ptr + cx));
+
+        /* Initialize the rows */
+        *(row_pointers + cx) = malloc (height * sizeof (png_bytep));
+        for (row = 0; row < height; row++) {
+            *(*(row_pointers + cx) + row) = malloc (png_get_rowbytes (*(png_ptr + cx), *(info_ptr + cx)));
+        }
+
+        /* Read the png */
+        png_read_image (*(png_ptr + cx), *(row_pointers + cx));
     }
-    memcpy (fb, fb_buf, finfo.smem_len);
+
+    /* Set slide to initial frame */
+    cx = 0;
+
+    do {
+        /* Draw to the screen */
+        memset (fb_buf, 0, finfo.smem_len);
+        for (row = 0; row < height && row < info.yres; row++) {
+            cx2 = 0;
+            for (col = 0; col < width && col < info.xres; col++) {
+                /* R */
+                *(fb_buf + position (row, col)) |= (*(*(*(row_pointers + cx) + row) + (cx2 + 0)) << 16) & 0xFF0000;
+                /* G */
+                *(fb_buf + position (row, col)) |= (*(*(*(row_pointers + cx) + row) + (cx2 + 1)) << 8) & 0xFF00;
+                /* B */
+                *(fb_buf + position (row, col)) |= *(*(*(row_pointers + cx) + row) + (cx2 + 2)) & 0xFF;
+                cx2 += 3;
+            }
+        }
+        memcpy (fb, fb_buf, finfo.smem_len);
+
+        /* Get input */
+        switch (getch ()) {
+        case 'q':
+        case 'Q':
+            quit = 1;
+            break;
+        case 'h':
+        case 'j':
+            cx = (cx > 0) ? cx - 1 : cx;
+            break;
+        case 'H':
+        case 'J':
+            cx = 0;
+            break;
+        case 'k':
+        case 'l':
+            cx = (cx + 1 < slide_count) ? cx + 1 : cx;
+            break;
+        case 'K':
+        case 'L':
+            cx = slide_count - 1;
+            break;
+        default:
+            break;
+        }
+    } while (!quit);
 
 close_png:
-    png_destroy_read_struct (&png_ptr, &info_ptr, &end_info);
+    for (cx = 0; cx < slide_count; cx++) {
+        png_destroy_read_struct ((png_ptr + cx), (info_ptr + cx), (end_info + cx));
+    }
 close_ncurses:
-    while (getch () != 'q');
-
     /* Close ncurses */
     curs_set (1);
     echo ();
     endwin ();
 
+    printf ("slides: %d\n", slide_count);
     printf ("widht: %d\n", width);
     printf ("height: %d\n", height);
     printf ("color type (%d), RGB (%d), RGBA (%d)\n", color_type, PNG_COLOR_TYPE_RGB, PNG_COLOR_TYPE_RGBA);
     printf ("bit depth: %d\n", bit_depth);
 
     /* Close the image file */
-    fclose (pic_file);
+    for (cx = 0; cx < slide_count; cx++) {
+        fclose (*(pic_file + cx));
+    }
+    free (pic_file);
+    free (fname);
 
     /* Close the framebuffer */
     free (fb_buf);
+    memset (fb, 0, finfo.smem_len);
     munmap (fb, finfo.smem_len);
     close (fb_file);
 
